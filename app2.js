e3776bf168e15d92378ad89a29716da5bf448fe2
Mansi sarkar <mansisarkar2000@gamil.com>
2020-12-13 16:54:52 +0530

    changed

--- app2.js
+++ app2.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use('/public', express.static(path.join(__dirname,'static')));
-app.set('view engine','ejs');
+const readline = require('readline');
+const r1 = readline.createInterface({input : process.stdin,
+                                    output : process.stdout}) ;
+                                    
+let num1 = Math.floor((Math.random()*10)+ 1);
+let num2 = Math.floor((Math.random()*10)+ 1);
 
-const people = require('./routes/people');
-app.use('/people',people);
+let ans = num1 + num2;
 
-app.listen(3000);
+r1.question(`what is the ${num1} + ${num2}??`,
+    (user) => {
+    if(user == ans){
+        r1.close();
+    }else{
+        r1.setPrompt('wrong answer\n');
+        r1.prompt();
+        r1.on('line',(user)=> {
+            if(user.trim() == ans){
+                r1.close();
+            }else{
+                r1.setPrompt("Wrong answer enter again\n");
+                r1.prompt();
+            }
+        })
+    }
+});
+
+r1.on('close',() => {
+    console.log('you got the correct answer\n');
+})
